25bff94d2828d513ad3caa4bbcfc577c0d339d8b
Walter-Britos1 <walterbritos418@gmail.com>
2024-03-30 01:07:47 -0300

    add hook config

--- challenge-dev/src/components/Home/Home.jsx
+++ challenge-dev/src/components/Home/Home.jsx
@@ -1,25 +1,8 @@
-import { useEffect, useState } from "react";
-import { ALL_CHARACTERS } from "../../graphql/queries";
-import { useQuery } from "@apollo/client";
+import useHome from '../../hooks/useHome';
 import SearchBar from "../SearchBar/SearchBar"
 
 const Home = () => {
-  const [characters, setCharacters] = useState([]);
-  const { loading, error, data, fetchMore } = useQuery(ALL_CHARACTERS, {
-    variables: { page: 1 },
-  });
-
-  useEffect(() => {
-    if (data) {
-      setCharacters((prev) => [...prev, ...data.characters.results]);
-      if (data && data.characters && data.characters.info && data.characters.info.next) {
-        fetchMore({
-          variables: { page: data.characters.info.next },
-        });
-      }
-      
-    }
-  }, [data, fetchMore]);
+  const { loading, error } = useHome();
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :{error.message}</p>;
---
+++ challenge-dev/src/hooks/useHome.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { ALL_CHARACTERS } from "../graphql/queries";
+import { useQuery } from "@apollo/client";
+
+const useHome = () => {
+  const [characters, setCharacters] = useState([]);
+  const { loading, error, data, fetchMore } = useQuery(ALL_CHARACTERS, {
+    variables: { page: 1 },
+  });
+
+  useEffect(() => {
+    if (data) {
+      setCharacters((prev) => [...prev, ...data.characters.results]);
+      if (data && data.characters && data.characters.info && data.characters.info.next) {
+        fetchMore({
+          variables: { page: data.characters.info.next },
+        });
+      }
+    }
+  }, [data, fetchMore]);
+
+  return { characters, loading, error };
+}
+
+export default useHome
