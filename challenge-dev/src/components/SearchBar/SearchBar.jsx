10ab711f0a33154b31854c7db1130594e4095d15
Walter-Britos1 <walterbritos418@gmail.com>
2024-03-29 23:39:19 -0300

    apply hooks

--- challenge-dev/src/components/SearchBar/SearchBar.jsx
+++ challenge-dev/src/components/SearchBar/SearchBar.jsx
@@ -1,44 +1,15 @@
-import { useState, useEffect } from 'react';
-import { useLazyQuery } from '@apollo/client';
-import { ALL_CHARACTERS, SEARCH_CHARACTER } from '../../graphql/queries';
+import useSearchBar from '../../hooks/useSearchBar'
 import Card from '../Card/Card';
 import Filter from "../Filters/Filter"
 
 const SearchBar = () => {
-  const [getAll, { data: allData }] = useLazyQuery(ALL_CHARACTERS);
-  const [search, { data: searchData }] = useLazyQuery(SEARCH_CHARACTER);
-  const [results, setResults] = useState([]);
-
-  useEffect(() => {
-    getAll({ variables: { page: 1 } });
-  }, [getAll]);
-
-  useEffect(() => {
-    if (allData) {
-      setResults(allData.characters.results);
-    }
-  }, [allData]);
-
-  const handleSearch = (event) => {
-    event.preventDefault();
-    const form = event.target;
-    const formData = new FormData(form);
-    const name = formData.get('name');
-    search({ variables: { name } });
-  };
-
-
-  useEffect(() => {
-    if (searchData) {
-      setResults(searchData.characters.results);
-    }
-  }, [searchData]);
+  const { handleSearch, setResults, results } = useSearchBar();
 
   return (
     <form onSubmit={handleSearch}>
       <input type="search" name="name" placeholder="Search character.." />
       <button type="submit">Search</button>
-      <Filter onFilterApply={setResults}/>
+      <Filter onFilterApply={setResults} />
       {results.map(character => (
         <Card key={character.id} character={character} />
       ))}
---
+++ challenge-dev/src/hooks/useSearchBar.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+import { useLazyQuery } from '@apollo/client';
+import { ALL_CHARACTERS, SEARCH_CHARACTER } from '../graphql/queries'
+
+const useSearchBar = () => {
+  const [getAll, { data: allData }] = useLazyQuery(ALL_CHARACTERS);
+  const [search, { data: searchData }] = useLazyQuery(SEARCH_CHARACTER);
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    getAll({ variables: { page: 1 } });
+  }, [getAll]);
+
+  useEffect(() => {
+    if (allData) {
+      setResults(allData.characters.results);
+    }
+  }, [allData]);
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const formData = new FormData(form);
+    const name = formData.get('name');
+    search({ variables: { name } });
+  };
+
+
+  useEffect(() => {
+    if (searchData) {
+      setResults(searchData.characters.results);
+    }
+  }, [searchData]);
+  return {
+    handleSearch,
+    setResults,
+    results,
+  }
+}
+
+export default useSearchBar;
