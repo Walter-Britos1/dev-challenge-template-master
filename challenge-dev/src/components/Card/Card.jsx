f23ebc95ef7823a524cb8e31db1513c385e33862
Walter-Britos1 <walterbritos418@gmail.com>
2024-03-31 13:20:47 -0300

    adding link for details

--- challenge-dev/src/components/Card/Card.jsx
+++ challenge-dev/src/components/Card/Card.jsx
@@ -1,8 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const Card = ({ character }) => {
   return (
     <div>
+      <Link to={`/${character.id}`}>
       <h2>{character.name}</h2>
       <img src={character.image} alt={character.name} />
+      </Link>
     </div>
   )
 };
