9f348a477cffc450572237047e1c8b13870d51f3
Walter-Britos1 <walterbritos418@gmail.com>
2024-03-31 13:12:30 -0300

    adding route for details

--- challenge-dev/src/App.jsx
+++ challenge-dev/src/App.jsx
@@ -1,15 +1,16 @@
-
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
-
+import Detail from './components/Detail/Detail'
 
 function App() {
-
-
-  return (
+  return ( 
     <div>
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/:id' element={<Detail />} />
+      </Routes>
     </div>
   )
 }
 
-export default App
+export default App;
