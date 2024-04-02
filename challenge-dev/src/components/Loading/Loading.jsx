b87144b379dc1f04067a7110341dd4c9430750ed
Walter-Britos1 <walterbritos418@gmail.com>
2024-04-01 23:01:52 -0300

    Fixing Styles

--- challenge-dev/src/components/Loading/Loading.jsx
+++ challenge-dev/src/components/Loading/Loading.jsx
@@ -4,7 +4,7 @@ const Loading = () => {
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
       <img className='w-64 h-64 object-cover mb-4' src={rickLoading} alt='Rick Loading...' />
-      <h1 className='text-white text-2xl'>Loandig...</h1>
+      <h1 className='text-white text-2xl text-center'>Loading...</h1>
     </div>
   )
 }
