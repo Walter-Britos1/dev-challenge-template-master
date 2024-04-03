d0c217a9590e7cfd32cbd1d1b8e09f781bf7af9c
Walter-Britos1 <walterbritos418@gmail.com>
2024-04-03 12:13:37 -0300

    change styles

--- challenge-dev/src/components/Error/Error.jsx
+++ challenge-dev/src/components/Error/Error.jsx
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom'
 const Error = ({ message }) => {
 
   return (
-    <div className="flex items-center justify-center h-screen">
-    <div className="bg-white p-8 rounded shadow-md w-1/3">
-      <h1 className="text-2xl font-bold mb-4">Error</h1>
-      <p className="mb-4">{message}</p>
-      <Link to='/' className="flex items-center text-blue-500 hover:underline">
+    <div className='flex items-center justify-center h-screen'>
+    <div className='bg-white p-8 rounded shadow-md w-1/3'>
+      <h1 className='text-2xl font-bold mb-4'>Error</h1>
+      <p className='mb-4'>{message}</p>
+      <Link to='/' className='flex items-center text-blue-500 hover:underline'>
         <CornerDownLeft className='h-6 w-6 mr-2' /> Go Back
       </Link>
     </div>
